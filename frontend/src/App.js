cf8983ac73851da0d54c4ea8d3b0362fc078bb78
Salas Uniagustiniana <salas@uniagustiniana.local>
2025-10-22 11:54:58 -0500

    primer commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserList from './components/userList';
+function App() {
+  return (
+    <div>
+      <h1>React + MySQL Example</h1>
+      <UserList />
+    </div>
+  );
+}
+export default App;
