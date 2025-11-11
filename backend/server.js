c630c3e41871ec91898f4ba93ffb7d3d8db9a748
Jesus <jesusmariavargaslogreira@gmail.com>
2025-11-11 18:07:46 -0500

    login usuarios

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const usuariosRoutes = require('./routes/usuarios');
+const loginRoutes = require('./routes/login');
+
 
 const app = express();
 
@@ -8,12 +10,12 @@ const PORT = 5001;
 app.use(cors());
 app.use(express.json());
 app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/login', loginRoutes);
 app.get('/', (req, res) => {
     res.json({
         message: 'API de Usuarios funcionando correctamente'
     });
 });
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en
-http://localhost:${PORT}`);
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
---
+++ frontend/src/components/loginUsuarios.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login({ onLoginSuccess }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.post('http://localhost:5001/api/login', { username, password });
+            setMessage(response.data.message);
+            onLoginSuccess(response.data.user);
+
+        } catch (error) {
+            console.error('Error:', error);
+            setMessage('Ha ocurrido un error');
+        }
+    };
+
+    return (
+        <div>
+            <header>
+                <h1>Login</h1>
+            </header>
+            <div>
+                <input
+                    type='text'
+                    placeholder='Usuario'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type='password'
+                    placeholder='Contraseña'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={handleSubmit}>
+                    Ingresar
+                </button>
+                {message && <p>{message}</p>}
+            </div>
+        </div>
+    );
+}
+
+export default Login;
